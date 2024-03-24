93a3b8eeb22e5268948fe069095a434e6490a788
kouzhao2333 <126130839+kouzhao2333@users.noreply.github.com>
2024-03-24 16:06:56 +0800

    Update Person.java

--- 04-phonebook/src/main/java/oy/tol/tra/Person.java
+++ 04-phonebook/src/main/java/oy/tol/tra/Person.java
@@ -8,7 +8,7 @@ public class Person implements Comparable<Person> {
         this.firstName = new String(person.firstName);
         this.lastName = new String(person.lastName);
     }
-    
+
     public Person(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
@@ -34,7 +34,7 @@ public class Person implements Comparable<Person> {
     /**
      * TODO: Implement the method below to return a hash value. It must be calculated from the
      * first and last name of the person.
-     * 
+     *
      * @return Hash value of the person.
      */
     @Override
